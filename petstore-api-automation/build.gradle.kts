77aaa9e00d06dd2c627e54b1c9ba450fc239bdc1
Jan Jurado <jandaniel.jurado@hotmail.com>
2025-02-21 16:51:02 -0500

    Initial Gradle, dependencies and structure set uo

--- petstore-api-automation/build.gradle.kts
+++ petstore-api-automation/build.gradle.kts
@@ -1,18 +1,31 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.9.0"
+    id("io.qameta.allure") version "2.11.2"
 }
 
-group = "com.automation.petstore"
-version = "1.0-SNAPSHOT"
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.cucumber:cucumber-java:7.14.0")
+    testImplementation("io.cucumber:cucumber-junit:7.14.0")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    testImplementation("net.serenity-bdd:serenity-cucumber:3.9.2")
+    testImplementation("net.serenity-bdd:serenity-core:3.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+
+    // Logging
+    implementation("org.slf4j:slf4j-simple:2.0.7")
 }
+
 
 tasks.test {
     useJUnitPlatform()
