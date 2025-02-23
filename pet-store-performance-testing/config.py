49aeb63509ffb465f1a211f0d3b8d2250e574822
Jan Jurado <jandaniel.jurado@hotmail.com>
2025-02-23 18:57:14 -0500

    Added project structured, config framework

---
+++ pet-store-performance-testing/config.py
@@ -0,0 +1,13 @@
+# API Base URL
+HOST = "http://localhost:8080/api/v3"
+
+# Load Test Parameters
+USERS = 10          # Number of concurrent users
+SPAWN_RATE = 2      # Users spawned per second
+RUN_TIME = "1m"     # Total test duration (e.g., 1m = 1 minute)
+
+
+# Wait Time Config (in seconds)
+WAIT_TIME_MIN = 1
+WAIT_TIME_MAX = 3
+
